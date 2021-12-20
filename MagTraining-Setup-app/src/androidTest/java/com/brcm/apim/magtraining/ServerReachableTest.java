1ba8df7bcdad333721de734cb3ea4e137d6b903a
Naveen <yerram.kumar@broadcom.com>
2021-12-20 10:15:31 +0530

    Added Run-time permission in Automation script

--- MagTraining-Setup-app/src/androidTest/java/com/brcm/apim/magtraining/ServerReachableTest.java
+++ MagTraining-Setup-app/src/androidTest/java/com/brcm/apim/magtraining/ServerReachableTest.java
@@ -1,20 +1,15 @@
 package com.brcm.apim.magtraining;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-
 import android.content.Context;
 import android.os.Build;
-
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
