b344083e8032ac6c26350ca4d902a69a342b78f1
Naveen <yerram.kumar@broadcom.com>
2022-01-04 10:27:17 +0530

    1) Removed unused code 2) Added comments for class and methods 3) Removed unnecessary comments.

--- MagTraining-Setup-app/src/androidTest/java/com/brcm/apim/magtraining/ServerReachableTest.java
+++ MagTraining-Setup-app/src/androidTest/java/com/brcm/apim/magtraining/ServerReachableTest.java
@@ -5,17 +5,24 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
 import android.content.Context;
 import android.os.Build;
+
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+/*This class is used to test whether the server is reachable or not */
 public class ServerReachableTest {
+
+    /* This method is called before the test, it will invoke/enable the runtime permissions, This also initiates Idling resource
+    object */
     @Before
     public void before() {
         IdlingRegistry.getInstance().register(CountingIdlingResourceSingleton.countingIdlingResource);
@@ -37,20 +44,21 @@ public class ServerReachableTest {
     public ActivityScenarioRule<MainActivity> mActivityRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
-    @Test
-    public void ServerReachableTest() {
 
+    /*This method is used to test "Server is reachable!"*/
+    @Test
+    public void Test01_ServerReachableTest() {
         onView(withId(R.id.textViewApiStatus))
                 .check(matches(withText(getResourceString(R.string.server_reachable))));
-
     }
 
+    /*This method is used to get the content from string.xml */
     private String getResourceString(int id) {
         Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         return targetContext.getResources().getString(id);
     }
 
-
+    /*This method is executed after the test, used to unregister Idling resource */
     @After
     public void unregisterIdlingResource() {
         IdlingRegistry.getInstance().unregister(CountingIdlingResourceSingleton.countingIdlingResource);
